491990bd5ab470dbed7ead4b2e0bb22b77d14178
Tiago P Blaszcyk <76741385+TiagoPBlaszcyk@users.noreply.github.com>
2021-08-01 22:04:43 -0300

    CRUDform

--- html/exercicios/form/serv.js
+++ html/exercicios/form/serv.js
@@ -7,8 +7,12 @@ app.use(express.json())
 
 app.post('/usuarios', (req,resp) => {
     console.log(req.body)
-    resp.send('<h1> Xau! ^~ </h1>')
+    resp.send('<h1>Usuario cadastrado</h1>')
 })
-
+app.post('/usuarios/:id', (req,resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Usuario alterado</h1>')
+})
 
 app.listen(5500)
