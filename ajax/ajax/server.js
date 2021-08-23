d1349277ff9d1f418e788295316560e7d63ac7aa
Tiago P Blaszcyk <76741385+TiagoPBlaszcyk@users.noreply.github.com>
2021-08-23 10:47:21 -0300

    RequestFullManual

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -5,6 +5,4 @@ app.use(express.static('.'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/legal', (req, res) => res.send('Fechou'))
-
 app.listen(8080, () => console.log('Executando...')) 
