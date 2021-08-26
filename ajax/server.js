2bdaa2204df01de0a45c705701cf6566629866d0
Tiago P Blaszcyk <76741385+TiagoPBlaszcyk@users.noreply.github.com>
2021-08-25 22:58:56 -0300

    AlterandoPastaDeUploadreworkspace

--- ajax/server.js
+++ ajax/server.js
@@ -9,7 +9,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, './upload')
+        callback(null, '.')
     },
     filename: function (req, file, callback) {
         callback(null, `${Date.now()}_${file.originalname}`)
